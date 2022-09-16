3ecb8a0515534d4669b2bd94117833c21a6cb8e5
Guilherme Azevedo Melim <guilherme.bobina@gmail.com>
2022-09-16 10:34:57 -0300

    Router concluido

--- futureeats/src/Paginas/Profile/Profile.js
+++ futureeats/src/Paginas/Profile/Profile.js
@@ -4,8 +4,7 @@ export const Profile = () => {
     return(
         <div>
             <h1>PARA O USUARIO SE CADASTRAS</h1>
-            <button>COMIDAS</button>
-            
+            <button>COMIDAS</button>          
         </div>
     )
 }
---
+++ futureeats/src/Paginas/RestaurantDetail/RestaurantDetail.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const RestauranteDetail = () => {
+    return(
+        <div>
+            <h1>TELA DETALHES DO RETAURANTE</h1>
+            <button>VOLTAR</button>
+            
+        </div>
+    )
+}
