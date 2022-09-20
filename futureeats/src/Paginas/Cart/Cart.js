248c39b1ab9c42492dd6ccad2ea9487b72013f0c
rafix923 <rafaellopes.dev@gmail.com>
2022-09-20 19:52:05 -0300

    Alterar cart e criar pasta components e CartOrder

--- futureeats/src/Paginas/Cart/Cart.js
+++ futureeats/src/Paginas/Cart/Cart.js
@@ -11,7 +11,9 @@ export const Cart = () => {
     return (
         <div>
             <HeaderCart>Meu carrinho</HeaderCart>
-
+            <>
+                <h2>SubTotal:R$ 0,00</h2>
+            </>
             <ButtonConfirmOrder>Confirmar</ButtonConfirmOrder>
         </div>
     )
