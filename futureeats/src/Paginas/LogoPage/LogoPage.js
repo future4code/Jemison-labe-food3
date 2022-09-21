6f1f37991b8b0af029dc7626d412d05c5cfd2acb
Guilherme Azevedo Melim <guilherme.bobina@gmail.com>
2022-09-20 23:40:06 -0300

    Login funcional

--- futureeats/src/Paginas/LogoPage/LogoPage.js
+++ futureeats/src/Paginas/LogoPage/LogoPage.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { goToLogin } from "../../Routes/Coordinator";
 
 export const LogoPage = () => {
+
+    const navigate = useNavigate()
+
+    const loginPage = () => {
+        goToLogin(navigate)
+    }
+
     return(
         <div>
             <h1>TELA INICIAL</h1>
-            <button>LOGIN</button>
+            <button onClick={() => loginPage()} >LOGIN</button>
             
         </div>
     )
