7014b319c5a42a55725deaa4d8413dedfc816385
unknown <glmaciosek@gmail.com>
2022-10-01 19:31:03 -0300

    atualizado com carrinho

--- futureeats/src/Paginas/RestaurantDetail/RestaurantDetail.js
+++ futureeats/src/Paginas/RestaurantDetail/RestaurantDetail.js
@@ -5,16 +5,20 @@ import { useState } from "react";
 
 export const RestauranteDetail = () => {
   const [restaurantList, setRestaurantList] = useState([]);
-  const [addButton, setAddButton] = useState(true);
-  const onClickAddButton = () => {
-    setAddButton(!addButton);
+  const [products, setSelectedProducts] = useState([]);
+
+  const addToCart = (item) => {
+    const selectedProducts = [...products, item];
+    setSelectedProducts(selectedProducts);
+    // console.log(selectedProducts)
+    alert("Produto Adicionado ao Carrinho");
   };
 
   useEffect(() => {
     const token =
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IndKU1RKVEhWRmV4RGdERWREMzNQIiwibmFtZSI6IkFzdHJvZGV2IiwiZW1haWwiOiJhc3Ryb2RldkBmdXR1cmU0LmNvbSIsImNwZiI6IjExMS4xMTEuMTExLTEzIiwiaGFzQWRkcmVzcyI6dHJ1ZSwiYWRkcmVzcyI6IlIuIERvbSBBdmVsYXIsIDEsIDIgLSBWaWxhIE4uIFZpdG9yaWEiLCJpYXQiOjE2NjQ0OTgwNTZ9.yjhv5NRDxXip9cZXbiL3CAAvhCUJ1uUvdUpO-KMdLrc";
     axios
-      .get(`${BASE_URL}/restaurants/1`, {
+      .get(`${BASE_URL}/restaurants/1}`, {
         headers: {
           auth: token,
         },
@@ -53,7 +57,7 @@ export const RestauranteDetail = () => {
             <span>{item.description}</span>
             <span>R${item.price}</span>
           </div>
-          <button onClick={() => onClickAddButton}>Adicionar</button>
+          <button onClick={() => addToCart(item)}>Adicionar</button>
         </div>
       ))}
 
