3c0bf57135295141b812ae29af6b7e60c3394833
rafix923 <rafaellopes.dev@gmail.com>
2022-10-01 08:37:07 -0300

    fix: se adicionar esse commit, adiciona div pai no retorno de renderização do componente restaurants, corrigindo o erro de renderização

--- futureeats/src/Paginas/Restaurants/Restaurants.js
+++ futureeats/src/Paginas/Restaurants/Restaurants.js
@@ -1,86 +1,85 @@
-import { useEffect,useState } from "react"
-import { ListRestaurant} from '../../Constants/Constants'
+import { useEffect, useState } from "react"
+import { ListRestaurant } from '../../Constants/Constants'
 import { ContainerRestaurants, Restaurants } from "./style";
 
-export const RestaurantsPage = () =>{
+export const RestaurantsPage = () => {
 
 
-    const [feed,setfeed] = useState([]);
+    const [feed, setfeed] = useState([]);
     useEffect(() => {
 
         ListRestaurant()
-        .then(data =>{
-            setfeed(data.restaurants)
-            console.log(data);
-        })
-        .catch((e) => {
-            console.log(e)
-        })
+            .then(data => {
+                setfeed(data.restaurants)
+                console.log(data);
+            })
+            .catch((e) => {
+                console.log(e)
+            })
 
-    },[]);
+    }, []);
 
-    const renderRestaurant = feed.map((res) =>{
-        return(
-
+    const renderRestaurant = feed.map((res) => {
+        return (
 
             <div>
-            key ={res.id}
-            name ={res.name}
-            img ={res.logUrl}
-            </div>
+                <div>
+                    key ={res.id}
+                    name ={res.name}
+                    img ={res.logUrl}
+                </div>
 
-            <ContainerRestaurants key ={res.id}> 
-            <img src={res.logoUrl}/>
-            name: {res.name}<br/>
-            Tempo: {res.deliveryTime}
-            Frete R${res.shipping},00
-            </ContainerRestaurants>
+                <ContainerRestaurants key={res.id}>
+                    <img src={res.logoUrl} />
+                    name: {res.name}<br />
+                    Tempo: {res.deliveryTime}
+                    Frete R${res.shipping},00
+                </ContainerRestaurants>
+            </div>
 
         )
     })
     console.log(renderRestaurant)
-    
+
     const [nameValue, setNameValue] = useState("")
     const [category, setCategory] = useState("")
 
     const updateNameValue = (event) => {
         setNameValue(event.target.value)
 
-    const filterCategory = feed.filter((item) => {
-        return(
-            item.category
+        const filterCategory = feed.filter((item) => {
+            return (
+                item.category
+            )
+        })
+
+
+        return (
+
+            <div>
+                <input placeholder="Restaurante"
+                    type='text' onChange={updateNameValue}
+                    value={nameValue}>
+                </input>
+
+                <button onClick={() => setCategory("")}>Todos</button>
+                <button onClick={() => setCategory("Árabe")}>Árabe</button>
+                <button onClick={() => setCategory("Asiática")}>Asiática</button>
+                <button onClick={() => setCategory("Baiana")} >Baiana</button>
+                <button onClick={() => setCategory("Hambúrguer")}>Hambúrguer</button>
+                <button onClick={() => setCategory("Carnes")}>Carnes</button>
+                <button onClick={() => setCategory("Petiscos")}>Petiscos</button>
+                <button onClick={() => setCategory("Italiana")}>Italiana</button>
+                <button onClick={() => setCategory("Mexicana")}>Mexicana</button>
+                <button onClick={() => setCategory("Sorvetes")}>Sorvetes</button>
+                <main>
+                    {renderRestaurant}
+                </main>
+                <div>
+                </div>
+                {renderRestaurant}
+            </div>
+
         )
-    })
-
-   
-   return(
-
-        <div>
-         <input placeholder="Restaurante" 
-         type='text' onChange={updateNameValue} 
-         value={nameValue}>
-        </input>
-
-         <button onClick={() => setCategory("")}>Todos</button>
-                    <button onClick={() => setCategory("Árabe")}>Árabe</button>
-                    <button onClick={() => setCategory("Asiática")}>Asiática</button>
-                    <button onClick={() => setCategory("Baiana")} >Baiana</button>
-                    <button onClick={() => setCategory("Hambúrguer")}>Hambúrguer</button>
-                    <button onClick={() => setCategory("Carnes")}>Carnes</button>
-                    <button onClick={() => setCategory("Petiscos")}>Petiscos</button>
-                    <button onClick={() => setCategory("Italiana")}>Italiana</button>
-                    <button onClick={() => setCategory("Mexicana")}>Mexicana</button>
-                    <button onClick={() => setCategory("Sorvetes")}>Sorvetes</button>
-<main>
-    {renderRestaurant}
-</main>
-        
-        </div>
-
-    <div>
-        {renderRestaurant}
-     </div>
-
-    )
-}
+    }
 }
