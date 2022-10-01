152b77cd46b01bfbc13d266a9cb1d16b6d785104
Adauto <adauto.matosjr@gmail.com>
2022-09-30 23:41:15 -0300

    home.category

--- futureeats/src/Paginas/Restaurants/Restaurants.js
+++ futureeats/src/Paginas/Restaurants/Restaurants.js
@@ -22,6 +22,7 @@ export const RestaurantsPage = () =>{
 
     const renderRestaurant = feed.map((res) =>{
         return(
+
             <div>
             key ={res.id}
             name ={res.name}
@@ -29,15 +30,43 @@ export const RestaurantsPage = () =>{
             </div>
         )
     })
-    
     console.log(renderRestaurant)
+    
+    const [nameValue, setNameValue] = useState("")
+    const [category, setCategory] = useState("")
+
+    const updateNameValue = (event) => {
+        setNameValue(event.target.value)
+
+    const filterCategory = feed.filter((item) => {
+        return(
+            item.category
+        )
+    })
+
+   
    return(
         <div>
-         
+         <input placeholder="Restaurante" 
+         type='text' onChange={updateNameValue} 
+         value={nameValue}>
+        </input>
+
+         <button onClick={() => setCategory("")}>Todos</button>
+                    <button onClick={() => setCategory("Árabe")}>Árabe</button>
+                    <button onClick={() => setCategory("Asiática")}>Asiática</button>
+                    <button onClick={() => setCategory("Baiana")} >Baiana</button>
+                    <button onClick={() => setCategory("Hambúrguer")}>Hambúrguer</button>
+                    <button onClick={() => setCategory("Carnes")}>Carnes</button>
+                    <button onClick={() => setCategory("Petiscos")}>Petiscos</button>
+                    <button onClick={() => setCategory("Italiana")}>Italiana</button>
+                    <button onClick={() => setCategory("Mexicana")}>Mexicana</button>
+                    <button onClick={() => setCategory("Sorvetes")}>Sorvetes</button>
 <main>
     {renderRestaurant}
 </main>
         
         </div>
     )
+}
 }
