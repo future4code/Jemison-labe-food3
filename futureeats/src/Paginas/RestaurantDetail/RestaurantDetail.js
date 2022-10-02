5abba1835feec52807bab3c194fe58e79f1f23db
unknown <glmaciosek@gmail.com>
2022-10-01 23:04:37 -0300

    detalhes do restaurante e styles atualizados

--- futureeats/src/Paginas/RestaurantDetail/RestaurantDetail.js
+++ futureeats/src/Paginas/RestaurantDetail/RestaurantDetail.js
@@ -2,16 +2,36 @@ import axios from "axios";
 import React, { useEffect } from "react";
 import { BASE_URL } from "../../Constants/Constants";
 import { useState } from "react";
-import { ContainerCard, Image} from "./styled";
+import { useNavigate } from "react-router-dom";
+import {
+  ContainerCard,
+  Image,
+  Name,
+  Description,
+  Price,
+  AddButton,
+  AdicionarText,
+  ContainerDiv,
+  FlexBox,
+  Category,
+  BoxPriceAdd,
+  Header,
+  RestaurantName
+} from "./style";
 
 export const RestauranteDetail = () => {
   const [restaurantList, setRestaurantList] = useState([]);
   const [products, setSelectedProducts] = useState([]);
+  const navigate = useNavigate();
+
+  const goToRestaurantsPage = () => {
+    navigate("/restaurants");
+  };
 
   const addToCart = (item) => {
     const selectedProducts = [...products, item];
     setSelectedProducts(selectedProducts);
-    // console.log(selectedProducts)
+    console.log(selectedProducts);
     alert("Produto Adicionado ao Carrinho");
   };
 
@@ -19,7 +39,7 @@ export const RestauranteDetail = () => {
     const token =
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IndKU1RKVEhWRmV4RGdERWREMzNQIiwibmFtZSI6IkFzdHJvZGV2IiwiZW1haWwiOiJhc3Ryb2RldkBmdXR1cmU0LmNvbSIsImNwZiI6IjExMS4xMTEuMTExLTEzIiwiaGFzQWRkcmVzcyI6dHJ1ZSwiYWRkcmVzcyI6IlIuIERvbSBBdmVsYXIsIDEsIDIgLSBWaWxhIE4uIFZpdG9yaWEiLCJpYXQiOjE2NjQ0OTgwNTZ9.yjhv5NRDxXip9cZXbiL3CAAvhCUJ1uUvdUpO-KMdLrc";
     axios
-      .get(`${BASE_URL}/restaurants/1}`, {
+      .get(`${BASE_URL}/restaurants/1`, {
         headers: {
           auth: token,
         },
@@ -34,38 +54,41 @@ export const RestauranteDetail = () => {
 
   return (
     <>
-      <header>
-        <span className="Title Text-Style">Restaurante</span>
-      </header>
       <span>Endereço</span>
-      {restaurantList.map((item, index) => (
-        <ContainerCard key={index}>
+      <ContainerDiv>
+        <Header>
+          <button onClick={goToRestaurantsPage}>Voltar</button>
+        </Header>
+        <RestaurantName>Restaurante</RestaurantName>
+        {restaurantList.map((item, index) => (
           <div>
-            <span>{item.category}</span>
+            <Category>{item.category}</Category>
+            <ContainerCard key={index}>
+              <FlexBox>
+                <Image
+                  width="150px"
+                  src={item.photoUrl}
+                  alt="miniatura do produto"
+                />
+                <div>
+                  <div>
+                    <Name>{item.name}</Name> <br />
+                    <Description>{item.description}</Description> <br />
+                  </div>
+                  <BoxPriceAdd>
+                    <Price>R${item.price}</Price>
+                    <AddButton>
+                      <button onClick={() => addToCart(item)}>
+                        <AdicionarText>Adicionar</AdicionarText>
+                      </button>
+                    </AddButton>
+                  </BoxPriceAdd>
+                </div>
+              </FlexBox>
+            </ContainerCard>
           </div>
-          <div>
-            <span>
-              <Image
-                src={item.photoUrl}
-                alt="miniatura do produto"
-              />
-            </span>
-          </div>
-          <div>
-            <span>{item.name}</span> <br/>
-            <span>{item.description}</span> <br/>
-            <span>R${item.price}</span>
-          </div>
-<<<<<<< HEAD
-          <button onClick={() => addToCart(item)}>Adicionar</button>
-        </div>
-=======
-          <button onClick={() => onClickAddButton}>Adicionar</button>
-        </ContainerCard>
->>>>>>> 25a85463552d64c4ac017b81c3132c2858936254
-      ))}
-
-      <button>VOLTAR</button>
+        ))}
+      </ContainerDiv>
     </>
-
-  )};
+  );
+};
